2d43eab71d24d10bb15f127223bfc74bf59521f6
bletelli <31071694+bletelli@users.noreply.github.com>
2017-08-17 10:30:56 +0200

    Update Passager.java

--- vol-persistence/src/main/java/vol/metier/model/Passager.java
+++ vol-persistence/src/main/java/vol/metier/model/Passager.java
@@ -10,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Version;
+// tell her that you love her, if you really love her...
 
 @Entity
 public class Passager {
